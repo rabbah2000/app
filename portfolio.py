973822a85e276317ac2b2deb92ebbacf570a1290
rabbah2000 <150491738+rabbah2000@users.noreply.github.com>
2024-07-10 13:17:45 +0100

    Create portfolio.py

---
+++ portfolio.py
@@ -0,0 +1,76 @@
+import streamlit as st
+
+# Accueil
+st.title("Rabbah Imrane")
+st.subheader("Élève Ingénieur en Génie de la Data")
+st.text("+212 708-258298 | RABAT, Agdal | rabbahimrane6@gmail.com")
+
+# À propos de moi
+st.header("À propos de moi")
+st.write("""
+Je suis un étudiant en première année à l’ENSIAS, spécialisé en ingénierie de la data. Passionné par l’analyse des
+données et le développement de solutions informatiques innovantes, je cherche à rejoindre une équipe dynamique où
+je pourrai contribuer avec mes compétences techniques et mon esprit d’équipe.
+""")
+
+# Compétences
+st.header("Compétences")
+st.write("""
+- **Programmation** : Python (Flask, Streamlit), SQL, PLSQL, Java (programmation orientée objet)
+- **Machine Learning et Traitement de données** : traitement d’image, traitement d’audio
+- **Systèmes d’exploitation** : Linux, Windows
+""")
+
+# Expériences
+st.header("Expériences")
+st.write("""
+- **Responsable data, AI CLUB RABAT, ENSIAS (2023)**
+  - Supervision des projets de data science et analyse des données pour divers cas d’étude.
+  
+- **Vice Team Leader, Enactus ENSIAS (2023)**
+  - Gestion d’une équipe de 5 membres pour développer des projets à impact social et innovant.
+""")
+
+# Projets
+st.header("Projets")
+st.write("""
+- **E-Service**
+  - Développement d’une application e-service avec analyse prédictive des besoins des utilisateurs. Mise en
+    place de fonctionnalités prédictives pour anticiper les besoins des utilisateurs basées sur l’historique et les tendances
+    de données. Utilisation d’algorithmes de machine learning pour personnaliser les recommandations de services.
+  
+- **Train**
+  - Conception et développement d’une base de données en SQL pour gérer les voyages en train.
+  
+- **Chatbot**
+  - Conception et développement d’un chatbot capable d’analyser le contenu de fichiers PDF pour fournir
+    des réponses contextuelles.
+  
+- **Securway**
+  - Participation au projet Securway, axé sur la prévention de la somnolence des conducteurs.
+""")
+
+# Éducation
+st.header("Éducation")
+st.write("""
+- **Élève Ingénieur 1ère année en Génie Informatique, L’Ecole Nationale Supérieure d’informatique et d’Analyse des Systèmes (ENSIAS) (2023)**
+  
+- **Classes préparatoires, Lycée technique Mohammedia (2021-2023)**
+""")
+
+# Langues
+st.header("Langues")
+st.write("""
+- **Anglais** : Conversational
+- **Français** : Niveau professionnel
+- **Arabe** : Langue maternelle
+""")
+
+# Centres d'intérêt
+st.header("Centres d'intérêt")
+st.write("""
+- Analyse de données
+- Systèmes de gestion de base de données
+- Machine learning
+- Nettoyage des données
+""")
